e35d52c84330314f3ea5ad432119c5f12706192c
Vinicius Ferreira <viniciuferreira32@gmail.com>
2021-11-14 21:06:37 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -11,16 +11,13 @@ Um simples timer com tema
 
 ## Design Desktop
 <br/>
-<!--<img src="imagens/design/desktop-design.jpg" alt="" srcset="" width="450" height="300">
-<img src="imagens/design/active-states-lightbox.jpg" alt="" srcset="" width="450" height="300">-->
+<img src="imagens/timer-Desktop.gif" alt="timer-Desktop" srcset="" width="550" height="300">
 <br/>
 <br/>
 
 # Design Mobile
 <br/>
-<!--<img src="imagens/design/mobile-design.jpg" alt="" srcset="" width="150" height="300">
-<img src="imagens/design/mobile-design-basket-filled.jpg" alt="" srcset="" 
-width="150" height="300">-->
+<img src="imagens/timer-Mobile.gif" alt="timer-Mobile" srcset="" width="220" height="300">
 <br/>
 <br/>
 
--- js/function.js
+++ js/function.js
@@ -1,22 +1,89 @@
 
 $(function(){
+    //Variavel
+    var numb = 10;
+
     //Funções
     init();
+    Themes();
+    
 
     function init(){
         $('button').click(function(){
-            Progress($('#circleProgress'));
-        })
-
-    //Barra de progresso em circulo
-    function Progress(el){
-        $(el).circleProgress({fill: {color:'navy'}})
-        .on('circle-animation-progress', function(event, progress, stepValue){
-            $(this).find('#number').text(String(stepValue.toFixed(2)) +'px')
-        })
-
         
+            Progress($('#circleProgress'));
+            
+            score();
+        })
+
+        //Barra de progresso em circulo
+        function Progress(el){
+            $(el).circleProgress({fill: {color:'navy'},animation:{duration:10000, easing:'circleProgressEasing'},animationStartValue: 1.0,reverse:true})
+        }
+
+        function score(){
+           const intervalTimer = setInterval(function(){
+               numb --;
+               $('#number').text(numb)
+               if(numb <= 0){
+                   $('#number').html('Finish').css('font-size','4.3rem');
+                   $('button').html('<i class="fas fa-redo-alt"></i>');
+            }
+           },1000);
+
+           if(numb < 10){
+               numb = 10;
+               $('#number').text(numb);
+               clearInterval(intervalTimer)
+               $('button').html('Start');
+           }
+           console.log($('#number').text())
+
+        }
+
+    }    
+
+    function Themes(){
+        //Funções
+        lightTheme();
+        darkTheme();
+        
+        function lightTheme(){
+            $('#light').click(function(){
+                $(this).css('background-color','white')
+                .css('color','#1e2140');
+                $('main').css('background-color','white');
+                $('#theme').css('border-color','#1e2140');
+                $('#box h2').css('color','#1e2140')
+                $('#number').css('color','#1e2140');
+                $('button').css('background-color','#1e2140')
+                .css('color','white');
+
+                //Dark
+                $('#dark').css('background-color','#1e2140')
+                .css('color','white')
+                
+            })
+        }
+
+        function darkTheme(){
+            $('#dark').click(function(){
+                $(this).css('background-color','white')
+                .css('color','#1e2140');
+                $('main').css('background-color','#1e2140');
+                $('#theme').css('border-color','white');
+                $('#box h2').css('color','white')
+                $('#number').css('color','white');
+                $('button').css('background-color','white')
+                .css('color','#1e2140')
+
+                //Light
+            $('#light').css('background-color','#1e2140')
+                .css('color','white')
+                
+        })
+        }
     }
-    }
+    
 
 })
